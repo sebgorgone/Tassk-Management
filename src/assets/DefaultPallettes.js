68ed1c6dbe0763a0bd2bb5e3ef3fb7d838224d4a
SGORGONE <sebgorgone@gmail.com>
2025-07-18 22:25:26 -0400

    pallette logic balls

--- src/assets/DefaultPallettes.js
+++ src/assets/DefaultPallettes.js
@@ -1,24 +1,24 @@
 export const pallette = [
    {
-      vibr: "cc2936",
-      dark: "08415c",
-      light: "6b818c",
-      altLight: "f1bf98",
-      Bright: "eee5e9"
+      vibr: "#cc2936",
+      dark: "#08415c",
+      light: "#6b818c",
+      altLight: "#f1bf98",
+      bright: "#eee5e9"
    },
    {
-      vibr: "4F0147",
-      dark: "11001C",
+      vibr: "#4F0147",
+      dark: "#11001C",
       light: "35012C",
-      altLight: "290025",
-      Bright: "3A015C"
+      altLight: "#290025",
+      bright: "#3A015C"
    },
    {
-      vibr: "F06449",
-      dark: "36382E",
-      light: "EDE6E3",
-      altLight: "DADAD9",
-      Bright: "5BC3EB"
+      vibr: "#F06449",
+      dark: "#36382E",
+      light: "#EDE6E3",
+      altLight: "#DADAD9",
+      bright: "#5BC3EB"
    },
 ]
 
--- src/shell.jsx
+++ src/shell.jsx
@@ -7,13 +7,16 @@ function Shell() {
   //state
   const [count, setCount] = useState(0)
 
-  let c = pallette[0];
+  let c = pallette[2];
+
+  console.log(c)
 
   //style
 
   const header = {
     fontFamily: "fontss",
-    fontSize: "1em",
+    fontSize: "2em",
+    background: c.dark
   }
 
   const body = {
@@ -24,6 +27,11 @@ function Shell() {
   return (
     <>
       <div className="headerBar" style={header}>hello</div>
+      <div className="headerBar" style={{background: c.bright}}></div>
+      <div className="headerBar" style={{background: c.light}}></div>
+      <div className="headerBar" style={{background: c.altLight}}></div>
+      <div className="headerBar" style={{background: c.vibr}}></div>
+
     </>
   )
 }
