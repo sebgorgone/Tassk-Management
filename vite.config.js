8844512d791c01ed4db23f2c5e165424acdbb827
SGORGONE <sebgorgone@gmail.com>
2025-07-25 10:15:49 -0400

    vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'https://github.com/sebgorgone/Tassk-Management',
+  base: '/Tassk-Management/',
   plugins: [react()],
 })
